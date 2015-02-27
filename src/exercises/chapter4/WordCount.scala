2d00318c2e2110f1edcb9537a3bb6d5d63778bdd
Ashkrit <ashkrit@gmail.com>
2015-02-27 23:59:12 +0800

    Chapter4 - Wip

--- src/exercises/chapter4/WordCount.scala
+++ src/exercises/chapter4/WordCount.scala
@@ -1,9 +1,25 @@
 package exercises.chapter4
 
+import scala.collection.mutable
+
 
 object WordCount {
 
   def wordCount(): Unit = {
+    val in = new java.util.Scanner(new java.io.File("c:/temp/wordcount.txt"))
+
+    val wordsCount = mutable.Map[String, Int]() withDefaultValue 0
+    while (in.hasNext) {
+      val word = in.next()
+      val count = wordsCount(word)
+      wordsCount += (word -> (count + 1))
+    }
+
+    println(wordsCount)
+
+  }
+
+  def wordCountInMutable(): Unit = {
     val in = new java.util.Scanner(new java.io.File("c:/temp/wordcount.txt"))
 
     var wordsCount = Map[String, Int]() withDefaultValue 0
@@ -20,5 +36,6 @@ object WordCount {
   def main(args: Array[String]) {
 
     wordCount()
+    wordCountInMutable()
   }
 }
